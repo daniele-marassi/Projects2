a2713cf1df3e32b3499757dfe603007e19389e8d
Daniele Marassi <daniele.marassi@gmail.com>
2024-02-29 23:51:42 +0100

    add mouse position, add move  mouse click

---
+++ c#/MousePosition/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MousePosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText(Path.Combine(System.IO.Path.GetDirectoryName(Application.ExecutablePath), "MousePosition.txt"), "");
+            while (true) 
+            {
+                System.Threading.Thread.Sleep(2000);
+                var position = Cursor.Position;
+
+                var positionText = "X:" + position.X.ToString() + " Y:" + position.Y.ToString();
+                Console.WriteLine(positionText);
+
+                using (StreamWriter w = File.AppendText(Path.Combine(System.IO.Path.GetDirectoryName(Application.ExecutablePath), "MousePosition.txt")))
+                {
+                    w.WriteLine(positionText);
+                }
+            }
+        }
+    }
+}
---
+++ c#/MoveMouseAndClick/Program.cs
@@ -0,0 +1,64 @@
+﻿using Additional;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoveMouseAndClick
+{
+    internal class Program
+    {
+        private class Parameters
+        { 
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Sleep { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var _params = new List<Parameters>() { };
+
+            try
+            {
+                var _arg = "";
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (_arg != "") _arg += " ";
+                    _arg += args[i];
+                }
+
+                //_arg = "[{\"X\":101, \"Y\":102, \"Sleep\":103},{\"X\":201, \"Y\":202, \"Sleep\":203}]"; //for test
+                _params = JsonConvert.DeserializeObject<List<Parameters>>(_arg);
+
+                if (_params == null) throw new Exception();
+
+                foreach (var param in _params)
+                {
+                    MoveAndClick(param);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Pass this JSON structure in the parameters: " + "[{\"X\":101, \"Y\":102, \"Sleep\":103},{\"X\":201, \"Y\":202, \"Sleep\":203}]", "MoveMouseAndClick");
+            }
+        }
+
+        private static void MoveAndClick(Parameters param)
+        {
+            System.Threading.Thread.Sleep(param.Sleep);
+
+            Cursor.Position = new Point(param.X, param.Y);
+
+            System.Threading.Thread.Sleep(100);
+
+            VirtualMouse.LeftClick();
+        }
+    }
+}
